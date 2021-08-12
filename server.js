4e6851561a469b03532d6f98712b990b2eee14d3
YirimS <yirimseck@gmail.com>
2021-08-11 22:03:45 -0700

    the start of my weather.js

--- server.js
+++ server.js
@@ -3,7 +3,36 @@
 console.log ('hello')
 
 // use require instead of import
-const request = require('require');
+// const request = require('require');
 
 // docs say this is required
+const express = require('express');
 const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+require('dotenv').config();
+const axios = require('axios');
+
+const PORT = process.env.PORT;
+
+const  weather = require ('./data/weather.json');
+
+
+app.get('/',(req, res) => {
+  res.send('whats up!')
+});
+
+// endpoint same applies for movies
+
+app.get('/weather',(req, res) => {
+  let lat = req.query.lat;
+  let lon = req.query.lon;
+  let searchQuery = req.query.searchQuery;
+
+});
+
+
+
+app.listen(PORT, () => console.log(`up on ${PORT}`));
